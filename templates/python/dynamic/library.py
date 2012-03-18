a990883215927ffaabd6390718d6900974de42d1
Steven Wallace <snwallace@gmail.com>
2012-03-17 19:17:03 -0500

    Added a library function to Python.
    serverConnect. It's used every game, so it should have been there to
    begin with.

--- templates/python/dynamic/library.py
+++ templates/python/dynamic/library.py
@@ -19,6 +19,9 @@ except OSError:
 library.createConnection.restype = c_void_p
 library.createConnection.argtypes = []
 
+library.serverConnect.restype = c_int
+library.serverConnect.argtypes = [c_void_p, c_char_p, c_char_p]
+
 library.serverLogin.restype = c_int
 library.serverLogin.argtypes = [c_void_p, c_char_p, c_char_p]
 
@@ -52,7 +55,7 @@ ${conversions[arg.type]}\
 %endfor
 # accessors
 
-#Globals 
+#Globals
 %for datum in globals:
 library.get${capitalize(datum.name)}.restype = ${conversions[datum.type]}
 library.get${capitalize(datum.name)}.argtypes = [c_void_p]
